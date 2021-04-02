8b9441e8739e0f11da23369a15608a320b9f62f8
Nemanjajekul <nemanja031111@gmail.com>
2021-04-03 00:25:03 +0200

    wwwww

---
+++ Frontend/prijavise.js
@@ -0,0 +1,9 @@
+const addButton = document.querySelector(".myButton");
+addButton.addEventListener("click", GetInputa);
+
+async function GetInputa()
+{
+    const ime = document.querySelector(".ime");
+//let ime=document.getElementById("ime");
+console.log(ime);
+}
