e9e1992f64448f4d37f1a9902e001a45b2d95700
Nemanjajekul <nemanja031111@gmail.com>
2021-04-03 00:46:40 +0200

    tfxty

--- Frontend/prijavise.js
+++ Frontend/prijavise.js
@@ -1,9 +1,10 @@
-const addButton = document.querySelector(".myButton");
+window.onload=function(){
+const addButton = document.querySelector(".zasto");
 addButton.addEventListener("click", GetInputa);
-
 async function GetInputa()
 {
     const ime = document.querySelector(".ime");
 //let ime=document.getElementById("ime");
 console.log(ime);
+}
 }
