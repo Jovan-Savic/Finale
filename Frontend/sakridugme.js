3dd78283c81167215e2329af5edb6344a0070fcf
Nemanjajekul <nemanja031111@gmail.com>
2021-04-03 17:33:37 +0200

    m

--- Frontend/sakridugme.js
+++ Frontend/sakridugme.js
@@ -3,5 +3,5 @@ if(localStorage.getItem('user')!=null)
     let a=document.getElementById("login");
     a.innerHTML="";
     let b=document.getElementById("register");
-    a.innerHTML="";
+    b.innerHTML="";
 }
