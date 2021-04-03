b672e2f8888116217604a0ece50ee979c06691d3
Nemanjajekul <nemanja031111@gmail.com>
2021-04-03 02:47:31 +0200

    3 ujutru je plz samo hocu da spavam taj slatki slatki odmor aaaa

---
+++ Frontend/molimtesamohocudaspavam.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+    const addButton = document.querySelector(".plzhelp");
+    //console.log(addButton);
+    addButton.addEventListener("click", GetInputa);
+    async function GetInputa()
+    {
+        let mei=document.querySelector(".meil");
+        mei=mei.Value;
+        let sif=document.querySelector(".bol");
+        sif=sif.value;
+        let lmei=document.querySelector(".lmeil");
+        let lpas=document.querySelector(".lpass");
+    }
+}
