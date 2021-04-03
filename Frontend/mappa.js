97fe0cff544cb4bc4f808f3b679f8583437261a5
Jovan-Savic <jovanzsavic@gmail.com>
2021-04-03 17:59:25 +0100

    KA

--- Frontend/mappa.js
+++ Frontend/mappa.js
@@ -1,28 +1,15 @@
 
 
-function nis(){
+function beograd(){
 
-    let db = await axios.get("/use/stari");
-    //console.log(db);
-    let korisnici = db.data.poruka;
+    var beograd = [];
+    beograd.push([44.7995,20.5])
+    beograd.push([44.7995,20.4750])
+    beograd.push([44.83938173896593, 20.406409967753195])
 
-
-
-
-
-
-}
-
-
-
-async function beograd(){
-
-    let db = await axios.get("/use/stari");
-    //console.log(db);
-    let korisnici = db.data.poruka;
-    console.log(korisnici);
-
-
+    beograd.forEach(ele=>{
+        L.marker(ele).addTo(mymap);
+    })
 
 
 
@@ -44,7 +31,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 
 
+function setMap(){
+    mymap = L.map('mappa').setView([44.8125, 20.4612], 13);
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'sk.eyJ1Ijoic3RlZmFuOTgiLCJhIjoiY2tuMWx6ZDR2MHhibDJvcGNxdnp3aHpzciJ9.Wp5qun3mr4nMQoWo4AQEyw'}).addTo(mymap);
+
+
+}
 
 
 
